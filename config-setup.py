60e173600e1e3cf35facd78625efc94d9c258c9c
Thomas Yager-Madden <thomas@tym-clio.local>
2016-05-02 13:06:42 -0500

    Add commit setup to entrypoint

---
+++ config-setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import os
+import shutil
+from configparser import ConfigParser
+
+
+def main():
+    if os.getenv('PGPASSWORD'):
+        pgpasswd = os.getenv('PGPASSWORD')
+        smtp_passwd = os.getenv('GMAILPASSWD')
+
+        config = ConfigParser()
+        config.read_file(open('/oao-warehouse-etl/luigi.cfg'))
+        config['core']['smtp_password'] = smtp_passwd
+        config['scheduler']['record_task_history'] = 'True'
+        config['task_history'] = {}
+        config['task_history']['db_connection'] = ('postgresql://etl_user:%s'
+                                                   '@postgres:5433/'
+                                                   'warehouse' % pgpasswd)
+        with open('/etc/luigi/luigi.cfg', 'w') as cf:
+            config.write(cf)
+    else:
+        shutil.copyfile('/oao-warehouse-etl/luigi.cfg', '/etc/luigi/luigi.cfg')
+
+if __name__ == '__main__':
+    main()
--- warehouse-entry.sh
+++ warehouse-entry.sh
@@ -11,6 +11,8 @@ fi
 mkdir -p /etc/luigi
 mv /oao-warehouse-etl/luigi-interface-logger.ini /etc/luigi/luigi-interface-logger.ini
 
+python3 /oao-warehouse-etl/config-setup.py
+
 # Create and set perms on log file, then start tailing it, so docker logs/ fluentd gets via STDOUT
 touch /tmp/luigi-interface.log
 chmod 777 /tmp/luigi-interface.log
